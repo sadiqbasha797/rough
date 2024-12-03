396de22c63a428784701749fadb4f4f1238a869d
sadiqbasha797 <msadiqbasha777@gmail.com>
2024-12-03 11:43:57 +0530

    org

--- routes/orgSubscriptionRoutes.js
+++ routes/orgSubscriptionRoutes.js
@@ -11,11 +11,11 @@ const {
     getSubscriptionCounts
 } = require('../controllers/orgSubscription');
 
-router.post('/create', adminAuth, createOrgSubscription);
-router.get('/getAll', adminAuth, getAllOrgSubscriptions);
-router.get('/get/:id', adminAuth, getOrgSubscription);
-router.put('/update/:id', adminAuth, updateOrgSubscription);
-router.delete('/delete/:id', adminAuth, deleteOrgSubscription);
-router.get('/checkExpired', adminAuth, manualCheckExpiredSubscriptions);
+router.post('/create',  createOrgSubscription);
+router.get('/getAll', getAllOrgSubscriptions);
+router.get('/get/:id',  getOrgSubscription);
+router.put('/update/:id',  updateOrgSubscription);
+router.delete('/delete/:id',  deleteOrgSubscription);
+router.get('/checkExpired',  manualCheckExpiredSubscriptions);
 router.get('/subscriptionCounts', getSubscriptionCounts);
 module.exports = router;    
